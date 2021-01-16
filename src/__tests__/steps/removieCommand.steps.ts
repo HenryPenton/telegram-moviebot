6aa412810eeb66e71528cc5ec9f95705809bee43
henry penton <henry.penton@gmail.com>
2021-01-16 18:15:22 +0000

    fixing tests on node 10

--- src/__tests__/steps/removieCommand.steps.ts
+++ src/__tests__/steps/removieCommand.steps.ts
@@ -10,37 +10,11 @@ import { IncomingMessage } from "../../types";
 const feature = loadFeature("./src/__tests__/features/removieCommand.feature");
 
 defineFeature(feature, (test) => {
-  const movieWithInfo = () => {
-    jest.spyOn(fetcher, "fetcher").mockResolvedValueOnce(filmWithInfo);
-  };
-
-  const setFilm = async () => {
-    command = setMovieCommand;
-    movieWithInfo();
-    await response(command, mockApi, state);
-  };
-
   const removie = async (index: number) => {
     command = removieCommand(index);
     await response(command, mockApi, state);
   };
 
-  const filmSelectionWasCorrect = async (index: number, expected: string) => {
-    expect(mockSendMessage).toHaveBeenNthCalledWith(index, {
-      chat_id: "some_chat_id",
-      text: expected,
-    });
-  };
-
-  const filmSelectionReset = async (index: number) => {
-    command = getMovieCommand;
-    await response(command, mockApi, state);
-
-    expect(mockSendMessage).toHaveBeenNthCalledWith(index, {
-      chat_id: "some_chat_id",
-      text: "No movies have been set yet",
-    });
-  };
   const response = async (
     command: IncomingMessage,
     mockApi: any,
@@ -56,22 +30,6 @@ defineFeature(feature, (test) => {
   beforeEach(() => {
     mockSendMessage.mockReset();
   });
-
-  const setMovieCommand: IncomingMessage = {
-    message: {
-      from: { first_name: "Joe" },
-      chat: { id: "some_chat_id" },
-      text: "/setmovie somefilmname",
-    },
-  };
-
-  const getMovieCommand: IncomingMessage = {
-    message: {
-      from: { first_name: "Joe" },
-      chat: { id: "some_chat_id" },
-      text: "/getmovies",
-    },
-  };
 
   const removieCommand = (index: number): IncomingMessage => ({
     message: {
@@ -106,12 +64,11 @@ defineFeature(feature, (test) => {
     then(
       /^the film with at position (.*) in the selection is removed$/,
       (index: number) => {
-        
         expect(mockSendMessage).toHaveBeenCalledWith({
           chat_id: "some_chat_id",
           text: `${index} removed from the selection`,
         });
-        
+
         expect(state.getMovies()).not.toContain(`${index}`);
       }
     );
