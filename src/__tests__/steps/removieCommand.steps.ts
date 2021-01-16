a1a498429b236ec9d8d853c50bbdcdbbfe5d9e1c
henry penton <henry.penton@gmail.com>
2021-01-16 18:19:36 +0000

    back to normal

--- src/__tests__/steps/removieCommand.steps.ts
+++ src/__tests__/steps/removieCommand.steps.ts
@@ -1,7 +1,5 @@
 import * as messageHandler from "../../messageHandler/messageHandler";
-
 import { State } from "../../State/State";
-
 import { defineFeature, loadFeature } from "jest-cucumber";
 import { IncomingMessage } from "../../types";
 
@@ -50,7 +48,7 @@ defineFeature(feature, (test) => {
         let count = numberOfMovies;
         while (count > 0) {
           count--;
-          state.setMovie("" + (numberOfMovies - count));
+          state.setMovie(`${numberOfMovies - count}`);
         }
       }
     );
@@ -67,7 +65,7 @@ defineFeature(feature, (test) => {
           text: `${index} removed from the selection`,
         });
 
-        expect(state.getMovies()).not.toContain("" + index);
+        expect(state.getMovies()).not.toContain(`${index}`);
       }
     );
   });
