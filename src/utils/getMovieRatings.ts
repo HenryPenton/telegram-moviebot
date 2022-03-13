445c9d291cfe9f0a27a9d4938f5149e99824b48d
Henry Penton <henry.penton@gmail.com>
2022-03-13 18:30:08 +0000

    refactor ternary

--- src/utils/getMovieRatings.ts
+++ src/utils/getMovieRatings.ts
@@ -1,15 +1,16 @@
 import { Movie } from "../fetcher/movie/movieFetcher";
 
-
 export const getMovieRatings = (movie: Movie): string | undefined => {
   let setMovieRating;
   if (movie.Ratings) {
     if (movie.Ratings.length > 0) {
-      setMovieRating = `(${
-        movie.Ratings[0].Source === "Internet Movie Database"
+      const firstRating = movie.Ratings[0].Source;
+      const source =
+        firstRating === "Internet Movie Database"
           ? "IMDb"
-          : movie.Ratings[0].Source
-      } Rating: ${movie.Ratings[0].Value})`;
+          : movie.Ratings[0].Source;
+
+      setMovieRating = `(${source} Rating: ${movie.Ratings[0].Value})`;
     }
   }
   return setMovieRating;
