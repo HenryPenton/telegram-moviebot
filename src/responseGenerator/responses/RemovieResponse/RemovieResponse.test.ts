2780891029fd741ed76fb6ec0a5630095bc75609
Henry Penton <henry.penton@gmail.com>
2022-03-13 17:21:09 +0000

    removie response test

--- src/responseGenerator/responses/RemovieResponse/RemovieResponse.test.ts
+++ src/responseGenerator/responses/RemovieResponse/RemovieResponse.test.ts
@@ -1,16 +1,11 @@
-import { Commands } from "../../../commands";
-import { helpDefinitions } from "../HelpResponse/helpDefinitions";
-import { HelpResponse } from "../HelpResponse/HelpResponse";
+import { State } from "../../../State/State";
+import { RemovieResponse } from "./RemovieResponse";
 
-describe("GetVotesResponse", () => {
-  test("responds with the help blurb", () => {
+describe("RemovieResponse", () => {
+  test("responds that it cannot remove a movie that is not in the selection", () => {
+    const state = new State();
     expect(
-      new HelpResponse(
-        helpDefinitions,
-        Object.keys(Commands)
-      ).generateResponse()
-    ).toEqual(
-      `\nmovie: Get information about a movie by name\nmovieyear: Get information about a movie by its year, where the format is /movieyear moviename YYYY\nmovieid: Get information about a movie by imdb id\nsetmovie: Sets a movie in the selection by name\nsetmovieyear: Sets a movie in the selection by its year, where the format is /movieyear moviename YYYY\nsetmovieid: Sets a movie in the selection by imdb id\ngetmovies: Get the movie selection\nmoviepoll: Get a movie poll in the form of a telegram vote!\nremovie: Remove a movie by the id given in /getmovies or by name - if a partial name is given, the first match will be removed\nreset: Resets the movie selection\ncleanup: Removes any duplicate movies from the selection\nvotes: Get the voting status on the current movie poll\nhelp: Get this list`
-    );
+      new RemovieResponse(state, "barry goes to hollywood").generateResponse()
+    ).toEqual(`Couldn't find that film in the selection`);
   });
 });
