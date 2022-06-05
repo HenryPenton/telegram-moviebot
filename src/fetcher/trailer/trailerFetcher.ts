0f665a0cfa094281a03c8bb50fbf74d8d837063c
Henry Penton <henry.penton@gmail.com>
2022-06-05 09:53:07 +0100

    missing async

--- src/fetcher/trailer/trailerFetcher.ts
+++ src/fetcher/trailer/trailerFetcher.ts
@@ -6,13 +6,14 @@ export type YoutubeResponse = {
   items: { id: { videoId: string } }[];
 };
 
-export const getTrailer = (movieName: string): Promise<Trailer> => {
-  return fetcher(
-    `https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_API_KEY}&part=snippet&q=${movieName}%20movie%20trailer`
-  )
-    .then((res) => {
-      const youtubeResponse = res as unknown as YoutubeResponse;
-      return `https://www.youtube.co.uk/watch?v=${youtubeResponse.items[0].id.videoId}`;
-    })
-    .catch(() => "");
+export const getTrailer = async (movieName: string): Promise<Trailer> => {
+  try {
+    const res = await fetcher(
+      `https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_API_KEY}&part=snippet&q=${movieName}%20movie%20trailer`
+    );
+    const youtubeResponse = res as unknown as YoutubeResponse;
+    return `https://www.youtube.co.uk/watch?v=${youtubeResponse.items[0].id.videoId}`;
+  } catch {
+    return "";
+  }
 };
